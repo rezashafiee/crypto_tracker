730b3c588a81fcd0a9f3b954777fe92478928e40
reza <mrshafiee@hotmail.com>
2025-07-11 12:12:26 +0200

    implement coin list screen

--- app/src/main/java/com/tilda/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/tilda/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
