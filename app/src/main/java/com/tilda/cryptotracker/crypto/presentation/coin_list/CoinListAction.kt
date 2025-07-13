7009a96bbfab19d4ac7eaf30e1fb762a81604e80
reza <mrshafiee@hotmail.com>
2025-07-13 12:20:26 +0200

    implement the viewModel

---
+++ app/src/main/java/com/tilda/cryptotracker/crypto/presentation/coin_list/CoinListAction.kt
@@ -0,0 +1,7 @@
+package com.tilda.cryptotracker.crypto.presentation.coin_list
+
+import com.tilda.cryptotracker.crypto.presentation.models.CoinUi
+
+sealed interface CoinListAction {
+    data class OnCoinClick(val coinId: CoinUi): CoinListAction
+}
---
+++ app/src/main/java/com/tilda/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,52 @@
+package com.tilda.cryptotracker.crypto.presentation.coin_list
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tilda.cryptotracker.core.domain.util.onSuccess
+import com.tilda.cryptotracker.crypto.domain.CoinDataSource
+import com.tilda.cryptotracker.crypto.presentation.models.toCoinUi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CoinListViewModel(
+    private val coinDataSource: CoinDataSource
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(CoinListState())
+    val state = _state
+        .onStart { loadCoins() }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000L),
+            initialValue = CoinListState()
+        )
+
+    fun onAction(action: CoinListAction) {
+        when (action) {
+            is CoinListAction.OnCoinClick -> {
+                // navigate to coin details
+            }
+        }
+    }
+
+    private fun loadCoins() {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(isLoading = true)
+            }
+            coinDataSource.getCoins().onSuccess { coins ->
+                _state.update { it ->
+                    it.copy(
+                        isLoading = false,
+                        coins = coins.map { coin -> coin.toCoinUi() }
+                    )
+                }
+            }
+        }
+    }
+}
